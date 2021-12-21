e7d4067b37f02b7a31721e356a6fd5a0c94e7c86
adimunteanu <adimunteanu13@gmail.com>
2021-12-21 12:35:45 +0200

    refactor: parse request data and optimize code

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from config import SECRET_KEY
 from security import authenticate, identity
@@ -16,6 +16,14 @@ items = [
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: (x['name'] == name), items), None)
@@ -25,13 +33,14 @@ class Item(Resource):
         if next(filter(lambda x: (x['name'] == name), items), None):
             return {'message', "An item with name '{}' already exists.".format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = next(filter(lambda x: (x['name'] == name), items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
