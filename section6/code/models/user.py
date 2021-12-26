cde5fe4550fb59cae2d1f2c29328608dadfad820
adimunteanu <adimunteanu13@gmail.com>
2021-12-26 12:12:09 +0200

    fix: typo

--- section6/code/models/user.py
+++ section6/code/models/user.py
@@ -6,7 +6,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
-    password = db.Column(db.string(80))
+    password = db.Column(db.String(80))
 
     def __init__(self, _id, username, password):
         self.id = _id
