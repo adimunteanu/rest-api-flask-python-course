89e439c2d2fda9cfa2b636ceb55a6540cd9115be
adimunteanu <adimunteanu13@gmail.com>
2021-12-28 12:54:06 +0200

    feat: create StoreModel

--- section6/code/resources/item.py
+++ section6/code/resources/item.py
@@ -12,6 +12,12 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank!"
     )
+    parser.add_argument(
+        'store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id!"
+    )
 
     @jwt_required()
     def get(self, name):
@@ -24,7 +30,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
@@ -39,7 +45,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
