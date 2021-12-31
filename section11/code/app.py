687111820ced6e5267ca65a7a7ac63f3a17a3347
adimunteanu <adimunteanu13@gmail.com>
2021-12-31 11:34:27 +0200

    feat: add flask_jwt_extended for auth

--- section11/code/app.py
+++ section11/code/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 from db import db
 
 from config import SECRET_KEY
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -22,7 +21,7 @@ def create_tables():
     db.create_all()
 
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
 
 
 api.add_resource(Store, '/store/<string:name>')
@@ -31,6 +30,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     db.init_app(app)
--- section11/code/resources/item.py
+++ section11/code/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required
 
 from models.item import ItemModel
 
@@ -22,7 +22,9 @@ class Item(Resource):
     @jwt_required()
     def get(self, name):
         item = ItemModel.find_by_name(name)
-        return item.json() if item else {'message': 'Item not found'}, 404
+        if item:
+            return item.json(), 200
+        return {'message': 'Item not found'}, 404
 
     def post(self, name):
         if ItemModel.find_by_name(name):
