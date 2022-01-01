b1b430e03563695df36378c136af8a4244ed1a9f
adimunteanu <adimunteanu13@gmail.com>
2022-01-01 18:42:16 +0200

    feat: added TokenRefresh resource

--- section11/code/resources/item.py
+++ section11/code/resources/item.py
@@ -26,6 +26,7 @@ class Item(Resource):
             return item.json(), 200
         return {'message': 'Item not found'}, 404
 
+    @jwt_required(fresh=True)
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message', "An item with name '{}' already exists.".format(name)}, 400
